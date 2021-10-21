a9bf43311e943eb7a37fa9abebf06fb728a3e2ab
kala <srikalapallekala568@gmail.com>
2021-10-21 18:47:47 +0530

    added new line

--- src/my_first/Sample.java
+++ src/my_first/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi this is kala");
+		System.out.println("hey i added a line");
 
 	}
 
